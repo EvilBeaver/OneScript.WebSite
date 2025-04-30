0b8e5bdec60f0fdf041a208e7aa5a9148c84d8f8
EvilBeaver <ovsiankin.aa@gmail.com>
2025-04-30 13:30:26 +0300

    Авторы с помощью внешнего сервиса

---
+++ frontend/docs/components/Authors.vue
@@ -0,0 +1,13 @@
+<script setup type="module" lang="ts">
+
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+}
+</script>
+
+<template>
+    <iframe 
+        style="width: 100%; height: 2000px; overflow: scroll; border: none"
+        src="https://readme-contribs.as93.net/contributors/EvilBeaver/OneScript?avatarSize=80&perRow=5&shape=circle&fontSize=18&margin=40">
+    </iframe>
+</template>
